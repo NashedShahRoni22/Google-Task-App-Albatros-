3b8dd7cf9a5e660c243196d38b254577e79309bf
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-12-29 00:44:17 +0600

    Add a task option done

--- src/Pages/AddTask.js
+++ src/Pages/AddTask.js
@@ -4,17 +4,59 @@ import Lottie from "lottie-react";
 import TaskAnim from "../Anim/man-with-task-list.json";
 import { AuthContext } from "../Context/AuthProvider";
 import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 const style = {
   height: 400,
 };
 const AddTask = () => {
   const { user } = useContext(AuthContext);
+  const handleAddTask =(e)=>{
+    e.preventDefault();
+    const form = e.target;
+    const task = form.task.value;
+    const image = form.image.files[0];
+    console.log(task, image);
+
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=7e02bcb75b861cc24bc21e5f016d85c0`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageData) => {
+        const taskDetails = {
+          task,
+          image: imageData.data.display_url
+        };
+        addTaskToDb(taskDetails);
+        form.reset();
+      })
+      .catch((e) => console.log(e));
+  }
+  const addTaskToDb = (taskDetails) => {
+    fetch("http://localhost:8000/task", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(taskDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("Task added successfully!");
+        }
+      });
+  };
   return (
     <div className="my-20 h-[80vh] relative">
       <h1 className="text-3xl text-center">Add a new task</h1>
-      <form className="mt-8 shadow-2xl p-8 rounded-3xl">
+      <form onSubmit={handleAddTask} className="mt-8 shadow-2xl p-8 rounded-3xl">
         <input
           type="file"
+          name="image"
           class="block text-sm text-slate-500
                 file:mr-4 file:py-2 file:px-4
                 file:rounded-full file:border-0
@@ -22,9 +64,9 @@ const AddTask = () => {
                 file:bg-violet-50 file:text-violet-700
                 hover:file:bg-violet-100 mb-4 cursor-pointer"
         />
-        <Input label="Task" />
+        <Input label="Task" name="task"/>
         {user?.uid ? (
-          <Button className="mt-4 w-full">Add</Button>
+          <Button className="mt-4 w-full" type="submit">Add</Button>
         ) : (
           <Link to="/login">
             <Button className="mt-4 w-full">Please login to add a task</Button>
